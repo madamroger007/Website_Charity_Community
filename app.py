351518997cc2016e8ee3cf36e02810134abdbc64
adam <adamsetiadijr07@gmail.com>
2023-12-12 03:45:52 +0700

    penangan pages

--- app.py
+++ app.py
@@ -780,6 +780,17 @@ def delete_project(project_id):
     }
     return jsonify(response)
 
+# *********************************************** Error Page *********************************************
+# Handler untuk kesalahan server internal (500)
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+# Handler untuk kesalahan server internal (404)
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
