734cd87af4cf2fc14a1ee535d136fb3959cdf5c7
madamroger007 <adamsetiadijr07@gmail.com>
2023-12-16 15:58:12 +0700

    menambahkan konfigurasi dan kode pendukung untuk setiap halaman

--- static/admin/dist/js/pages/dashboards/dashboard1.js
+++ static/admin/dist/js/pages/dashboards/dashboard1.js
@@ -1,14 +1,16 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
     /* initialize the calendar
     -----------------------------------------------------------------*/
 
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+    let calendarEl = document.getElementById('calendar');
+    let calendar = new FullCalendar.Calendar(calendarEl, {
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
         editable: true,
         droppable: true, // this allows things to be dropped onto the calendar
@@ -23,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     calendar.render();
     getTotal()
     getDonate()
-    getTemperature() 
+    getTemperature()
+    getNews() 
 });
 
 // Operasion DOM Jquery
@@ -36,11 +39,7 @@ function getTotal() {
         success: function (response) {
             // Perbarui tabel dengan data pengguna baru
             InfoCard(response);
-            console.log("berhasil")
-            console.log(response.timestamp_users)
-            console.log("berhasil")
-
-
+            
 
         },
         error: function (error) {
@@ -52,13 +51,15 @@ function getTotal() {
 
 function InfoCard(total) {
     // Mengubah format tanggal dari "YYYY-MM-DD-HH-mm-ss" menjadi "DD MMMM YYYY, HH:mm:ss"
-    // let time_update_user = 
+    setTimeout(function() {
+        console.log("Fungsi ini dijalankan setelah 2 detik");
+    
     let temp = `
     <li class="feed-item">
     <div class="feed-icon bg-info"><i class="mdi mdi-account-multiple-plus"></i></div> Total ${total.total_users} User  <span class="ms-auto font-12 text-muted">${moment(total.timestamp_users).fromNow()}</span>
 </li>
 <li class="feed-item">
-    <div class="feed-icon bg-success"><i class="mdi mdi-currency-usd"></i></div> Total $${total.total_donation} Donate <span class="ms-auto font-12 text-muted">${moment(total.timestamp_donations).fromNow()}</span>
+    <div class="feed-icon bg-success"><i class="mdi mdi-currency-usd"></i></div> Total ${toRupiah(total.total_donation, { symbol: 'IDR', floatingPoint: 0 })} Donate <span class="ms-auto font-12 text-muted">${moment(total.timestamp_donations).fromNow()}</span>
 </li>
 <li class="feed-item">
     <div class="feed-icon bg-warning"><i class="mdi mdi-library-books"></i></div> Total ${total.total_news} News <span class="ms-auto font-12 text-muted">${moment(total.timestamp_news).fromNow()}</span>
@@ -69,6 +70,7 @@ function InfoCard(total) {
 </li>
     `
     $("#information-total").empty().append(temp)
+}, 5000); // Waktu diatur dalam milidetik (1000 ms = 1 detik)
 }
 
 // Get donate
@@ -78,9 +80,9 @@ function getDonate() {
         url: "/get_donate",
         success: function (response) {
             // Perbarui tabel dengan data pengguna baru
-     
-            updateTableContent(response.donation)
            
+            updateTableDonate(response.donation)
+
 
         },
         error: function (error) {
@@ -90,41 +92,49 @@ function getDonate() {
 
 }
 
-function updateTableContent(donation) {
+function updateTableDonate(donation) {
     // Tambahkan data pengguna baru ke tabel
-    for (let index = 0; index < donation.length; index++) {
-        let donate = donation[index];
-        let nama = donate.username.substring(0, 2).toUpperCase();
-        let row = `
-        <tr class="pt-2">
-        <td class="d-flex align-items-center justify-content-center"><p>1</p></td>
-        <td>
-            <div class="d-flex align-self-start">
-                <div class="m-r-10 " style="margin-top: -10px; " >
-                <a
-                class="btn btn-circle d-flex btn-info text-white">${nama}</a>
-                  
+    setTimeout(function() {
+        console.log("Fungsi ini dijalankan setelah 2 detik");
+        $('#donation-form').empty();
+        for (let index = 0; index < 10; index++) {
+            let donate = donation[index];
+          
+            let nama = donate.username.substring(0, 2).toUpperCase();
+            let row = `
+            <tr class="pt-2">
+            <td class="d-flex align-items-center justify-content-center"><p>${(index + 1)}</p></td>
+            <td>
+                <div class="d-flex align-self-start">
+                    <div class="m-r-10 " style="margin-top: -10px; " >
+                    <a
+                    class="btn btn-circle d-flex btn-info text-white">${nama}</a>
+                      
+                    </div>
+                    <div class="">
+                        <h4 class="m-b-0 font-16">${donate.username}</h4>
+                    </div>
                 </div>
-                <div class="">
-                    <h4 class="m-b-0 font-16">${donate.username}</h4>
-                </div>
-            </div>
-        </td>
-        <td>${donate.email}</td>
-        <td>${donate.phone}</td>
-        <td>${donate.bank_account}</td>
-        <td>
-            <label class="label label-danger fw-bolder">${donate.country}</label>
-        </td>
-        <td>
-        <h5 class="m-b-0">${toRupiah(donate.donation_amount,{symbol: 'IDR', floatingPoint: 0}) }</h5>
-        </td>
-        <td>${moment(donate.date).fromNow()}</td>
-    </tr>
-           
-`;
-        $('#donation-form').append(row);
-    }
+            </td>
+            <td>${donate.email}</td>
+            <td>${donate.phone}</td>
+            <td>${donate.bank_account}</td>
+            <td>
+                <label class="label label-danger fw-bolder">${donate.country}</label>
+            </td>
+            <td>
+            <h5 class="m-b-0">${toRupiah(donate.donation_amount, { symbol: 'IDR', floatingPoint: 0 })}</h5>
+            </td>
+            <td>${moment(donate.date).fromNow()}</td>
+        </tr>
+               
+    `;
+            $('#donation-form').append(row);
+        }
+    
+    
+    }, 5000); 
+ 
 }
 
 
@@ -140,7 +150,7 @@ function getTemperature() {
         url: `https://api.weatherapi.com/v1/current.json?key=${API}&q=${city}&aqi=no`,
         success: function (response) {
             // Perbarui tabel dengan data pengguna baru
-           Temperature(response)
+            Temperature(response)
 
         },
         error: function (error) {
@@ -151,22 +161,10 @@ function getTemperature() {
 
 }
 
-function getDay(){
-    const today = new Date();
 
-    // Daftar nama hari dalam Bahasa Inggris
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    // Mendapatkan indeks hari dalam seminggu (0 = Minggu, 1 = Senin, ..., 6 = Sabtu)
-    const dayIndex = today.getDay();
-    // Mendapatkan nama hari berdasarkan indeks
-    const dayName = daysOfWeek[dayIndex];
-    return dayName
-}
-
-function Temperature(response){
-   const {cloud,wind_mph,humidity,pressure_in} = response.current
-   const {country,name} = response.location
+function Temperature(response) {
+    const { cloud, wind_mph, humidity, pressure_in } = response.current
+    const { country, name } = response.location
 
     let temp = `
     <h4 class="card-title">Temp Guide</h4>
@@ -221,4 +219,84 @@ function Temperature(response){
     </ul>
     `
     $("#temperatur").empty().append(temp)
+}
+
+// Get name
+function getDay() {
+    const today = new Date();
+
+    // Daftar nama hari dalam Bahasa Inggris
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    // Mendapatkan indeks hari dalam seminggu (0 = Minggu, 1 = Senin, ..., 6 = Sabtu)
+    const dayIndex = today.getDay();
+    // Mendapatkan nama hari berdasarkan indeks
+    const dayName = daysOfWeek[dayIndex];
+    return dayName
+}
+
+// Get News
+function getNews() {
+    $.ajax({
+        type: 'GET',
+        url: "/get_news",
+        success: function (response) {
+            // Perbarui tabel dengan data pengguna baru
+            console.log(response.news.length)
+
+
+                getCardNews(response.news)
+
+
+            console.log("succes news")
+
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+
+}
+
+function getCardNews(news){
+    setTimeout(function() {
+        $("#card-news").empty()
+        console.log("Fungsi ini dijalankan setelah 2 detik");
+        for (let index = 0; index < 2; index++) {
+            let data = news[index];
+            let nama = data.username.substring(0, 2).toUpperCase();
+    
+            let temp = 
+            `
+            <div class="d-flex flex-row comment-row">
+            <div class="p-2 " >
+                    <a
+                    class="btn btn-circle d-flex btn-info text-white">${nama}</a>
+                      
+                    </div>
+            <div class="comment-text active w-100">
+                <h6 class="font-medium">${data.username}</h6>
+                <div class="pb-2">
+                <img src="/static/${data.img}" alt="" style="width: 250px; height: 150px;" />
+            </div>
+                <span class="m-b-15 d-block">${data.description}</span>
+                <div class="comment-footer ">
+                    <span class="text-muted float-end">${moment(data.date).fromNow()}</span>
+                    <span class="label label-success label-rounded">${data.topic}</span>
+                    <span class="action-icons active">
+                        <a href="/dashboard/admin/news"><i class="ti-pencil-alt"></i></a>
+                       
+                    </span>
+                </div>
+            </div>
+        </div>
+            `
+        
+        $("#card-news").append(temp)
+        }
+},5000)
+
+   
+   
+
 }
