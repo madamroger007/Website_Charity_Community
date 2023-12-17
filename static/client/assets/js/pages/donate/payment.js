7c5642c9ca20dbb0e14bac2248107ccb1328d247
madamroger007 <adamsetiadijr07@gmail.com>
2023-12-17 13:42:27 +0700

    menambah js untuk pendukung client payment.html seperti menambahkan DOM dan AJAX

---
+++ static/client/assets/js/pages/donate/payment.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    getNews() 
+})
+
+
+
+//********************* Get News
+// Get News
+function getNews() {
+    $.ajax({
+        type: 'GET',
+        url: "/get_news",
+        success: function (response) {
+            // Perbarui tabel dengan data pengguna baru
+ 
+            getFooterNews(response.news)
+
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+
+}
+
+
+function getFooterNews(news){
+
+    $("#news-footer").empty()
+
+    for (let index = 0; index < 3; index++) {
+        let berita = news[index];
+        
+        let load =`
+        <div class='post-row d-flex justify-content-center align-items-center' id="load-card" style='width: 70px;
+        height: 70px;'>
+        <div class="loader-img " ></div> 
+        </div>
+        `
+        let temp = `
+        <div class="post-row rounded-3">
+        <div class="image">
+          <img src="/static/${berita.img}"  alt="" />
+        </div>
+        <div class="detail">
+          <p>${berita.title}</p>
+        </div>
+      </div>
+    
+    `
+
+    $("#news-footer").append(load)
+    setTimeout(function () {
+        console.log("Waktu telah berlalu setelah 2 detik!");
+        $("#load-card").remove();
+        $("#news-footer").append(temp)
+    }, 5000);
+
+  
+    }
+
+}
+
+// Button klik image
+$(document).ready(function () {
+    $('.button-donate-value').click(function () {
+        $('#image-donate').attr('src', $(this).data('src'));
+        $('#donate-amount').val($(this).data('value'));
+        
+    });
+
+
+    // Handle dropdown change event
+  
+});
+
+
+//****************** POST function
+
+
