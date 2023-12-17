e2d2209e2615753fd252e6e55542a978739eab8d
madamroger007 <adamsetiadijr07@gmail.com>
2023-12-17 13:42:51 +0700

    menambah js untuk pendukung client news.html seperti menambahkan DOM dan AJAX

---
+++ static/client/assets/js/pages/news/news.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    getNews() 
+})
+
+//********************* GET TOTAL
+
+
+
+//********************* Get News
+// Get News
+function getNews() {
+    $.ajax({
+        type: 'GET',
+        url: "/get_news",
+        success: function (response) {
+            // Perbarui tabel dengan data pengguna baru
+
+            getFooterNews(response.news)
+            getCardNews(response.news)
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+
+}
+
+function getCardNews(news){
+    $("#card-news-container").empty()
+
+    for (let index = 0; index < news.length; index++) {
+        let berita = news[index];
+        let load =`
+        <div class='card-news d-flex justify-content-center align-items-center' id="load-card" style='height:400px;'>
+        <div class="loader-img " ></div> 
+        </div>
+        `
+         let temp = `
+    <div class="col-lg-4 col-md-6 card-news ">
+    <div class="serv-cove rounded bg-white ">
+      <img src="/static/${berita.img}" alt="" class='img-news '/>
+      <div class="p-2">
+        <h5 class="mt-3 fs-7 fw-bold">
+          ${berita.title}
+        </h5>
+        <p class='description'> ${berita.title}</p>
+        <span class="fs-8">22 May 2015</span>
+        <span class="float-end fs-8"
+          ><i class="bi bi-person"></i> Sam Anderson</span
+        >
+      </div>
+    </div>
+  </div>
+    `
+    $("#card-news-container").append(load)
+    setTimeout(function () {
+        console.log("Waktu telah berlalu setelah 2 detik!");
+        $("#load-card").remove();
+        $("#card-news-container").append(temp)
+    }, 5000);
+}
+}
+
+function getFooterNews(news){
+
+    $("#news-footer").empty()
+
+    for (let index = 0; index < 3; index++) {
+        let berita = news[index];
+        
+        let load =`
+        <div class='post-row d-flex justify-content-center align-items-center' id="load-card" style='width: 70px;
+        height: 70px;'>
+        <div class="loader-img " ></div> 
+        </div>
+        `
+        let temp = `
+        <div class="post-row rounded-3">
+        <div class="image">
+          <img src="/static/${berita.img}"  alt="" />
+        </div>
+        <div class="detail">
+          <p>${berita.title}</p>
+        </div>
+      </div>
+    
+    `
+
+    $("#news-footer").append(load)
+    setTimeout(function () {
+        console.log("Waktu telah berlalu setelah 2 detik!");
+        $("#load-card").remove();
+        $("#news-footer").append(temp)
+    }, 5000);
+
+  
+    }
+
+}
+
